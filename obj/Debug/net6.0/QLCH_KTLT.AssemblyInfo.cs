cd8427280e7c1a9dc4021d570ef07adfda9596a6
DcThai <congthai0103b1@gmail.com>
2024-04-14 22:37:03 +0700

    Testing

--- obj/Debug/net6.0/QLCH_KTLT.AssemblyInfo.cs
+++ obj/Debug/net6.0/QLCH_KTLT.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QLCH_KTLT")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff1e4a5c14f0821ddec31b1a69f0997f9b874935")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6afcbbcdc2b879bf33da24e6496c5b736632b35")]
 [assembly: System.Reflection.AssemblyProductAttribute("QLCH_KTLT")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QLCH_KTLT")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
